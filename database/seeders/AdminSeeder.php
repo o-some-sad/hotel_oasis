b5c7ea9a5b340983703860e0cddcba5a91ddeced
ilawy <next.mohammed.amr@gmail.com>
2025-04-18 14:54:30 +0200

    feat(cli): add AdminSeeder for creating initial admin for the system

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //TODO: implement AdminSeeder logic after designing database structure
+        // email: admin@admin.com
+        // password: 123456
+        throw new \Exception('To be implemented');
+    }
+}
