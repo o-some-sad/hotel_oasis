92bc1b3fb066f5045881c65b146437e1d1721c98
ilawy <next.mohammed.amr@gmail.com>
2025-04-20 14:35:00 +0200

    feat: add initial admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
@@ -15,6 +17,19 @@ class AdminSeeder extends Seeder
         //TODO: implement AdminSeeder logic after designing database structure
         // email: admin@admin.com
         // password: 123456
-        throw new \Exception('To be implemented');
+        echo "creating admin";
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $admin = User::firstOrCreate([
+            'email' => 'admin@admin.com',
+        ], [
+            'id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('123456'),
+            'is_approved' => true,
+            'email_verified_at' => now(),
+            'created_by' => 1,
+            'approved_by' => 1
+        ]);        
     }
 }
