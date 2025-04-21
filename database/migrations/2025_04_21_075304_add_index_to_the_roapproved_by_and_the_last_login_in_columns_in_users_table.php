5eff4e748010d314c28f15d8f98fd256304c4b11
Esraa <esraakhalifa5@gmail.com>
2025-04-21 09:58:15 +0200

    chore: add indexes to Users table (approved_by, last_login_in, role)

---
+++ database/migrations/2025_04_21_075304_add_index_to_the_roapproved_by_and_the_last_login_in_columns_in_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            // Adding index to the approved_by column
+            $table->index('approved_by', 'approved_by_index');
+            // Adding index to the last_login_in column
+            $table->index('last_login_in', 'last_login_in_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
