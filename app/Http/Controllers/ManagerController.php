f90ee28d0a17e223c34211df5b881c31dad1aa67
Esraa <esraakhalifa5@gmail.com>
2025-04-21 11:49:26 +0200

    feat: add manager state to UserFactory and create ManagerSeede r

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ManagerController extends Controller
 {
@@ -18,8 +19,38 @@ class ManagerController extends Controller
 
     {
         //
-        $managers = User::where('role', 'manager')->get();
-        dd($managers);
-        return view('manager.index', compact('managers'));
+        
+        // dd($managers);
+        return Inertia::render('Manager/Index', [
+            'managers' => User::select([
+                'id',
+                'name',
+                'email',
+                'national_id',
+                'avatar_img',
+                'mobile',
+                'country',
+                'gender',
+                'is_approved',
+                'banned_at',
+                'created_at',
+            ])->where('role', 'manager')
+            ->latest()
+            ->paginate(10),
+        ]);
+    //     return response()->json(
+    //         User::select([
+    //             'id',
+    //             'name',
+    //             'email',
+    //             'national_id',
+    //             'avatar_img',
+    //             'mobile',
+    //             'country',
+    //         ])->where('role', 'manager')
+    //         ->latest()
+    //         ->paginate(10)
+    //     );
+    // }
     }
 }
