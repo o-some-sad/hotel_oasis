14ca46530d0c05e72fb74f2ed707e3779113bd41
AhmedRamadan <ahmedramadanmohamedoweis@gmail.com>
2025-04-24 18:45:44 +0200

    Implement My Reservations (Client: view reservations).

--- resources/js/pages/ClientReservations/index.vue
+++ resources/js/pages/ClientReservations/index.vue
@@ -2,6 +2,13 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { BreadcrumbItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
+import Table from './Table.vue';
+
+const props = defineProps<{
+    data: any;
+    links: any;
+}>();
+
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -17,8 +24,8 @@ const breadcrumbs: BreadcrumbItem[] = [
 <template>
     <AppLayout :breadcrumbs="breadcrumbs" title="Client Reservations">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-           <i>PLEASE IMPLEMENT ME :)</i>
-           <Link :href="route('my-reservations.create')">Create Reservation</Link>
+           
+            <Table :links="[]" :data="props.data" />
         </div>
     </AppLayout>
 </template>
